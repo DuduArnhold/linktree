b30cefce93827b86bff0419fdec95207a9c437e2
José Eduardo Arnhold <duduarnhold2@gmail.com>
2023-10-07 16:27:52 -0300

    Feat: Montando tela de erro 404

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Home } from './pages/home';
 import { Admin } from './pages/admin';
 import { Login } from './pages/login';
 import { Network } from './pages/networks';
+import { ErrorPage } from "./pages/notfound";
 
 import { Private } from "./routes/private";
 
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
   {
     path: "/admin/social",
     element: <Private> <Network/> </Private>
+  },
+  {
+    path: "*",
+    element: <ErrorPage/>
   }
 
 
